374f3da58abb84ad002b5cacc8e8a558cbab5657
davento <daniela.vento@utec.edu.pe>
2022-05-06 20:31:35 -0500

    graph update interphase working

--- graph.py
+++ graph.py
@@ -6,15 +6,24 @@ from networkx.drawing.nx_pydot import graphviz_layout
 from networkx.readwrite import json_graph
 import json
 
-
 def save_json(filename, graph):
     g = graph
     g_json = json_graph.node_link_data(g)
     json.dump(g_json, open(filename, 'w'), indent=2)
 
+def read_json(filename):
+    with open(filename) as f:
+        js_graph = json.load(f)
+    return json_graph.node_link_graph(js_graph)
 
-T = nx.balanced_tree(2, 5)
-pos = graphviz_layout(T, prog="dot")
-save_json("grafo.json", T)
-nx.draw(T, pos)
-plt.show()
+try:
+    while(True):
+        T = read_json("grafo.json")
+        pos = graphviz_layout(T, prog="dot")
+        nx.draw(T, pos)
+        plt.show
+        plt.pause(0.001)
+        plt.clf()
+        
+except KeyboardInterrupt:
+    pass
